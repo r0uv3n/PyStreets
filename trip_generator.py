529b8bf3114ed373764001d3d21cf77bbabc62d7
Henry Ruben Fischer <henryrubenfischer@gmail.com>
2018-12-21 11:16:10 +0100

    Correct trip_generator docstring

--- trip_generator.py
+++ trip_generator.py
@@ -1,8 +1,18 @@
 from random import sample
 
 
-def generate_trips(number_of_residents, potential_origins, potential_goals):
-    """This class creates the appropriate number of residents and manages the test_trips"""
+def generate_trips(number_of_residents: int, potential_origins, potential_goals):
+    """
+    Distribute the residents over the potential_origins and set random goals (out of the potential_goals) for them.
+    Args:
+        number_of_residents: number of residents to be distributed -> number of trips to be generated
+        potential_origins: potential origin nodes
+        potential_goals: potential goal nodes
+
+    Returns:
+        A dict with the potential_origins as keys and lists of destinations / goals as the values. The total number
+        of all destinations should be equal to the number_of_residents.
+    """
     trips = dict()
     number_of_goals = 0
     for i in range(0, number_of_residents):
